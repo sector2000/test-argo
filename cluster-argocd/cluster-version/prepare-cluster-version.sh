3c9505e0852da8e70f7b776fce5b54b7d13f4fd7
Giulio Cardillo <giulio.cardillo@gmail.com>
2024-03-22 12:36:45 +0100

    Add image signatures for disconnected clusters

--- cluster-argocd/cluster-version/prepare-cluster-version.sh
+++ cluster-argocd/cluster-version/prepare-cluster-version.sh
@@ -24,15 +24,26 @@ if ! ([[ "${OCP_VERSION}" =~ ^4\.[0-9]{1,2}\.[0-9]{1,2}$ ]] && curl -k -If https
   echo "Invalid Openshift version: ${OCP_VERSION}"
   exit 1
 fi
-echo "The provided Openshift Verison is valid"
+echo "The provided Openshift Version is valid"
 
 CHANNEL=stable-$(echo ${OCP_VERSION} | awk -F. '{print $1"."$2}')
 IMAGE=$(oc adm release info quay.io/openshift-release-dev/ocp-release:${OCP_VERSION}-${ARCHITECTURE} | sed -n 's/Pull From: //p')
+DIGEST=${IMAGE#*@}
+DIGEST_ALGORITHM=${DIGEST%%:*}
+DIGEST_ENCODED=${DIGEST#*:}
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  SIGNATURE_BASE64=$(curl -s "https://mirror.openshift.com/pub/openshift-v4/signatures/openshift/release/${DIGEST_ALGORITHM}=${DIGEST_ENCODED}/signature-1" | base64)
+else
+  SIGNATURE_BASE64=$(curl -s "https://mirror.openshift.com/pub/openshift-v4/signatures/openshift/release/${DIGEST_ALGORITHM}=${DIGEST_ENCODED}/signature-1" | base64 -w0)
+fi
 
 echo -n "Generating helm values file..."
 cat << EOF > ${PWD}/values.yaml
 channel: ${CHANNEL}
 release_image: ${IMAGE}
 openshift_version: ${OCP_VERSION}
+digest_algorithm: ${DIGEST_ALGORITHM}
+digest_encoded: ${DIGEST_ENCODED}
+signature_base64: ${SIGNATURE_BASE64}
 EOF
 echo " Done"
